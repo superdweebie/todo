85f64b04b3097e832f57378c05185e34f5dcfae0
Tim Roediger <superdweebie@gmail.com>
2014-04-08 06:12:21 +1000

    more experiments

--- start.js
+++
@@ -1,1 +0,0 @@
-require('shakedown')([__dirname]);
--- widgets/list/List.js
+++
@@ -1,9 +0,0 @@
-import Controller from 'sd/widgets/Controller';
-
-class List extends Controller {
-    constructor() {
-
-    }
-}
-
-export List
